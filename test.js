998481cbea83b45068dfbd4c3139272d67bfffc3
NK-2004 <146760458+NK-2004@users.noreply.github.com>
2025-06-01 14:43:18 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
 
+// test.js
+const http = require('http');
+
+http.get('http://localhost:3000', (res) => {
+  console.log(`Status Code: ${res.statusCode}`);
+}).on('error', (err) => {
+  console.error('Test failed:', err.message);
+});
